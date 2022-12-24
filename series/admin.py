afc45367d2e0bb00319a170219b051fe0395ddd8
ariahmad123 <94441253+ariahmad123@users.noreply.github.com>
2022-12-24 23:35:36 +0800

    Update admin.py

--- series/admin.py
+++ series/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+class AnimepopularAdmin(admin.ModelAdmin):
+    list_display = ('animeId', 'animeTitle', 'animeImg', 'releasedDate', 'animeUrl')
+
+class DetailanimeAdmin(admin.ModelAdmin):
+    list_display = ('animeId','animeTitle', 'type', 'releasedDate', 'status','otherNames', 'synopsis', 'animeImg', 'totalEpisodes')
+
+class TopratedanimeAdmin(admin.ModelAdmin):
+    list_display = ('animeId', 'animeTitle', 'animeImg', 'latestEpisode', 'animeUrl')
+
+class MovieAnimeAdmin(admin.ModelAdmin):
+    list_display = ('animeId', 'animeTitle', 'animeImg', 'releasedDate', 'animeUrl')
+
+admin.site.register(Animepopular, AnimepopularAdmin)
+admin.site.register(Detailanime, DetailanimeAdmin)
+admin.site.register(Topratedanime, TopratedanimeAdmin)
+admin.site.register(MovieAnime, MovieAnimeAdmin)
